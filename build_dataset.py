c45afc3e9d7824807f2f9c1347bf50c6a10faf80
Aditya Yadav <aky6895@gmail.com>
2023-07-13 23:45:46 +0000

    black changes

--- build_dataset.py
+++ build_dataset.py
@@ -1,21 +1,35 @@
 from vocabulary import LetterVocabulary
 import torch
 
-def convert_source_target_list_to_tensors(source_target_pair, vocab_index, init_token, eos_token):
+
+def convert_source_target_list_to_tensors(
+    source_target_pair, vocab_index, init_token, eos_token
+):
     tensors = []
     for pair in source_target_pair:
         tensor_pair = []
         for sentence in pair:
-            tensor = torch.LongTensor([vocab_index[init_token]] + [vocab_index[ch] for ch in sentence] + [vocab_index[eos_token]])
+            tensor = torch.LongTensor(
+                [vocab_index[init_token]]
+                + [vocab_index[ch] for ch in sentence]
+                + [vocab_index[eos_token]]
+            )
             tensor_pair.append(tensor)
         tensors.append(tensor_pair)
     return tensors
 
 
 class BuildDataset:
-    def __init__(self, LetterVocabulary, source_file='train.en', target_file='train.de'):
+    def __init__(
+        self, LetterVocabulary, source_file="train.en", target_file="train.de"
+    ):
         self.letter_vocab = LetterVocabulary()
         self.letter_vocab.build_vocabulary(source_file, target_file)
 
     def __call__(self):
-        return convert_source_target_list_to_tensors(self.letter_vocab.source_target_list, self.letter_vocab.ch2ix, self.letter_vocab.init_token, self.letter_vocab.eos_token)
+        return convert_source_target_list_to_tensors(
+            self.letter_vocab.source_target_list,
+            self.letter_vocab.ch2ix,
+            self.letter_vocab.init_token,
+            self.letter_vocab.eos_token,
+        )

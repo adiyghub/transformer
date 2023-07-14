75e21204826e05896eab0bfa4de8dbc88d7e21cf
Aditya Yadav <aky6895@gmail.com>
2023-07-14 06:15:00 +0000

    added mask function

---
+++ utils.py
@@ -0,0 +1,12 @@
+import torch
+
+def construct_mask(source, target, source_pad_idx, target_pad_idx):
+    
+    source_pad_mask = (source != source_pad_idx).unsqueeze(1).unsqueeze(2)
+    target_pad_mask = (target != target_pad_idx).unsqueeze(1).unsqueeze(2)
+
+    target_length = target.shape[1]
+    target_sequence_mask = torch.tril(torch.ones((target_length, target_length))).bool()
+
+    target_mask = target_sequence_mask & target_pad_mask
+    return source_pad_mask, target_mask

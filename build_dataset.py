b6957cf4ef6162f665ae5b319ce5ab03d66f9fc9
Aditya Yadav <aky6895@gmail.com>
2023-07-14 05:37:47 +0000

    cleanup class initialization

--- build_dataset.py
+++ build_dataset.py
@@ -24,12 +24,16 @@ class BuildDataset:
         self, LetterVocabulary, source_file="train.en", target_file="train.de"
     ):
         self.letter_vocab = LetterVocabulary()
-        self.letter_vocab.build_vocabulary(source_file, target_file)
+        self.source_file = source_file
+        self.target_file = target_file
 
-    def __call__(self):
-        return convert_source_target_list_to_tensors(
+        self.letter_vocab.build_vocabulary(self.source_file, self.target_file)
+        
+        self.ch2ix = self.letter_vocab.ch2ix 
+        self.sentence_pairs_dataset = convert_source_target_list_to_tensors(
             self.letter_vocab.source_target_list,
             self.letter_vocab.ch2ix,
             self.letter_vocab.init_token,
             self.letter_vocab.eos_token,
         )
+    

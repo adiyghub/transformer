90727a7b5c77875921dc095cc8d7c15154239360
adiyghub <adiyghub@gmail.com>
2023-07-14 21:29:00 +0000

    added padding

--- build_dataset.py
+++ build_dataset.py
@@ -1,8 +1,9 @@
 from vocabulary import TokenVocabulary
 import torch
 import re
+import torch.nn.functional as F
 
-def convert_source_target_list_to_tensors(
+def convert_source_target_list_to_tokens(
     source_target_pair, vocab_index, init_token, eos_token
 ):
     tensors = []
@@ -24,21 +25,36 @@ class BuildDataset:
         self, TokenVocabulary, source_file="train.en", target_file="train.de"
     ):
         self.token_vocab = TokenVocabulary()
+        self.pad_token = self.token_vocab.pad_token
         self.source_file = source_file
         self.target_file = target_file
 
         self.token_vocab.build_vocabulary(self.source_file, self.target_file)
         
         self.ch2ix = self.token_vocab.ch2ix
-        print("Length of ch2ix after building vocabulary:", len(self.ch2ix))
         self.vocab_size = len(set(self.token_vocab.ch2ix.values())) 
          
-        self.sentence_pairs_dataset = convert_source_target_list_to_tensors(
+        self.tokenized_sentences = convert_source_target_list_to_tokens(
             self.token_vocab.source_target_list,
             self.token_vocab.ch2ix,
             self.token_vocab.init_token,
             self.token_vocab.eos_token,
         )
-        self.max_len = max([max([len(sentence[0]) for sentence in self.sentence_pairs_dataset]), 
-                    max([len(sentence[1]) for sentence in self.sentence_pairs_dataset])])
+        self.max_len = max([max([len(sentence[0]) for sentence in self.tokenized_sentences]), 
+                    max([len(sentence[1]) for sentence in self.tokenized_sentences])])
+
+        padded_sources = [
+            F.pad(src, (0, self.max_len  - src.size(0)), value=self.ch2ix[self.token_vocab.pad_token]) for src, _ in self.tokenized_sentences
+            ]
+        
+        padded_targets = [
+            F.pad(tgt, (0, self.max_len  - tgt.size(0)), value=self.ch2ix[self.token_vocab.pad_token])
+            for _, tgt in self.tokenized_sentences
+            ]
+            
+        self.sequence = torch.stack([torch.stack(padded_sources), torch.stack(padded_targets)], dim=1)
+
+        
+
+        
     
